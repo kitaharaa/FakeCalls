e198604a6da09413b782f90de0a9be593bae900e
amogus3000 <bvalentin1415@gmail.com>
2022-06-15 22:02:22 +0300

    final version of activity_call.xml

--- app/src/main/java/com/hfad/fakecalls/MainActivity.java
+++ app/src/main/java/com/hfad/fakecalls/MainActivity.java
@@ -2,12 +2,16 @@ package com.hfad.fakecalls;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
     TimePicker timePicker;
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,14 @@ public class MainActivity extends AppCompatActivity {
 
         timePicker = (TimePicker) findViewById(R.id.timePicker);
         timePicker.setIs24HourView(true);
+
+        submitButton = findViewById(R.id.button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CallActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
