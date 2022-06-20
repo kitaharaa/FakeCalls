20cc54b8f63e1c971cf48cebd570eff1442f7a36
amogus3000 <bvalentin1415@gmail.com>
2022-06-20 13:55:31 +0300

    added data transfer between MainActivity and CallActivity.

--- app/src/main/java/com/hfad/fakecalls/MainActivity.java
+++ app/src/main/java/com/hfad/fakecalls/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-
     TimePicker timePicker;
-    Button submitButton;
+    int imageId;
+    String characterName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +25,81 @@ public class MainActivity extends AppCompatActivity {
 
         timePicker = (TimePicker) findViewById(R.id.timePicker);
         timePicker.setIs24HourView(true);
+    }
 
-        submitButton = findViewById(R.id.button);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CallActivity.class);
-                startActivity(intent);
-            }
-        });
+    public void onSpaceCowIconClicked(View view) {
+        onSelectedIconCLicked(R.drawable.space_cow, R.string.space_cow_name);
+    }
+
+    public void onPrimeIconClicked(View View) {
+        onSelectedIconCLicked(R.drawable.prime, R.string.prime_name);
+    }
+
+    public void onBorisIconClicked(View view) {
+        onSelectedIconCLicked(R.drawable.boris, R.string.boris_name);
+    }
+
+    public void onSelectedIconCLicked(int imageId, int characterNameId) {
+        this.imageId = imageId;
+        characterName = getString(characterNameId);
+
+        Toast toast = Toast.makeText(this, "Обраний рятівник: "
+                + this.characterName, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void onSubmitButtonClicked(View view) {
+        long delayTime = countingDelayTime();
+
+        EditText enterNumber = (EditText) findViewById(R.id.enter_number);
+        String phoneNumber = enterNumber.getText().toString();
+
+        if (phoneNumber.matches("") || imageId == 0) {
+            Toast toast = Toast.makeText(this,
+                    "Введи всі данні!", Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            Intent intent = new Intent(MainActivity.this, CallActivity.class);
+            intent.putExtra("phoneNumber", phoneNumber);
+            intent.putExtra("imageId", imageId);
+            intent.putExtra("characterName", characterName);
+
+            Toast toast = Toast.makeText(this,"Герой: "
+                            + characterName + "\n"
+                            + "Номер: " + phoneNumber + "\n"
+                            + "Приблизний час до дзвінка: "
+                            + TimeUnit.MILLISECONDS.toSeconds(delayTime) + "сек.",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(intent);
+                    enterNumber.setText("");
+                }
+            },delayTime);
+        }
+    }
+
+    private long countingDelayTime() {
+        int hour = timePicker.getHour();
+        int minutes = timePicker.getMinute();
+        long selectedTimeInMillis = TimeUnit.HOURS.toMillis(hour)
+                + TimeUnit.MINUTES.toMillis(minutes);
+
+        Calendar calendar = Calendar.getInstance();
+        int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+        int currentMinute = calendar.get(Calendar.MINUTE);
+        int currentSecond = calendar.get(Calendar.SECOND);
+        long currentTimeInMillis = TimeUnit.HOURS.toMillis(currentHour)
+                + TimeUnit.MINUTES.toMillis(currentMinute)
+                + TimeUnit.SECONDS.toMillis(currentSecond);
+
+        if (selectedTimeInMillis > currentTimeInMillis) {
+            return (selectedTimeInMillis - currentTimeInMillis);
+        } else {
+            return (currentTimeInMillis - selectedTimeInMillis) + selectedTimeInMillis;
+        }
     }
 }
